8a62d8973ab419b2a2265768e2fca7972cdc52a2
Paul Snow <snow.paul@gmail.com>
2019-05-19 19:48:45 -0500

    simplification of the algorithm

--- lxrhash.go
+++ lxrhash.go
@@ -19,39 +19,31 @@ func (w LXRHash) Hash(src []byte) []byte {
 	// The intital offset into the lookup table is the length of the input.
 	// This prevents concatenation attacks, which adds to the protection from
 	// the reduction pass.
-	var offset = int64(len(src))
+	var lastStage = int64(len(src)) ^ w.Seed ^ int64(w.HashSize)
 	// We keep a series of previous states, and roll them along through each
 	// byte of source processed.
-	var last1, last2, last3 int64
+	var stages [5]int64
+	v := w.ByteMap[lastStage%w.MapSize]
 
-	v := byte(offset)
-	var idx1, idx2 int64
+	step := func(i int, v2 int64) int64 {
+		lastStage = v2 ^ int64(i)<<16 ^ lastStage
+		for i, stage := range stages {
+			ui := uint64(i)
+			stages[i] = stage<<(8+ui) ^ stage>>(1+ui) ^ lastStage
+			lastStage = stage ^ lastStage<<5
+		}
 
-	step := func(i int, v2 int64) {
-		// combine the l values, the previous offset, and the hashes[i]
-		offset = last1<<7 ^ last2<<3 ^ last3<<9 ^ offset<<8 ^ offset>>1 ^ idx2 ^ int64(v)
+		v = w.ByteMap[uint64(lastStage)%uint64(w.MapSize)] ^ v
 
-		// Take the byte from source (v2) and map it through the lookup table
-		// using the offset being maintained, and the rolling lastX values
-		idx1 = int64(uint64(idx1^offset^v2) % uint64(w.MapSize))
-		v = w.ByteMap[idx1] ^ v
-
-		// Roll the set of last values, leaving lingering influences from past
-		// values.
-		last3 = last2>>2 ^ last3
-		last2 = last1<<3 ^ last2
-		last1 = int64(v) ^ v2 ^ last1<<1
-
-		idx2 = idx2 ^ hashes[uint32(i)%w.HashSize]
+		return hashes[uint32(i)%w.HashSize]
 	}
 
-	// Pass through the source bytes, building up lastX values, hashes[], and offset
 	for i, v2 := range src {
 		step(i, int64(v2))
 		// Set one of the hashes[] using the last rolling value, the input byte v2,
 		// the mapped byte v, and the previous hashes[] value
-		hashes[uint32(i)%w.HashSize] = last3 ^ int64(v^v2) ^ idx2
-
+		hashes[uint32(i)%w.HashSize] = lastStage ^
+			int64(v^w.ByteMap[uint64(stages[0]+int64(v2))%uint64(w.MapSize)])
 	}
 
 	// Reduction pass
@@ -66,7 +58,7 @@ func (w LXRHash) Hash(src []byte) []byte {
 	for i, h := range hashes {
 		step(i, h)
 		// Set a byte
-		idx2 := int64(uint64(int64(v)^offset) % uint64(w.MapSize))
+		idx2 := int64(uint64(int64(v)^lastStage) % uint64(w.MapSize))
 		bytes[i] = w.ByteMap[idx2]
 	}
 
