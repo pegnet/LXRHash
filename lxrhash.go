331ffac7590c54e7dbbb50bff71705745a225d6f
Paul Snow <snow.paul@gmail.com>
2019-07-04 15:13:29 -0500

    Simplified the coding, more byte accesses

--- lxrhash.go
+++ lxrhash.go
@@ -17,16 +17,22 @@ func (lx LXRHash) Hash(src []byte) []byte {
 	// and combine it with the state we are building up.
 	var as = lx.Seed
 	// We keep a series of states, and roll them along through each byte of source processed.
-	var s1, s2 uint64
+	var s1, s2, s3 uint64
 	// Since MapSize is specified in bits, the index mask is the size-1
 	mk := lx.MapSize - 1
 
+	B := func(v uint64) uint64 { return uint64(lx.ByteMap[v&mk]) }
+	b := func(v uint64) byte { return byte(B(v)) }
+
 	// Define a function to move the state by one byte.
 	step := func(v2 uint64) {
-		s1 = s1 ^ as ^ v2 ^ uint64(lx.ByteMap[(as^v2<<9)&mk])<<4
-		s2 = s1<<23 ^ s1>>5 ^ s2<<17 ^ s2>>3 ^ uint64(lx.ByteMap[(s2^v2<<9)&mk])<<11
-		as = s2<<29 ^ s2>>7 ^ as<<37 ^ as>>1 ^ uint64(lx.ByteMap[(s1^v2<<9)&mk])<<13
-		s1, s2 = s2, s1
+		s1 = s1<<9 ^ s1>>7 ^ as ^ B(as)<<7^B(s1^v2)<<3 ^ B(as^s1)<<17^ B(v2^as>>11^s1)<<23
+		as = as<<17 ^ as>>1 ^ s1 ^ B(as^v2)<<9^B(s1)<<8 ^ B(as^s1)<<23^ B(v2^as>>17^as^s1)<<17
+		s1 = s1<<7 ^ s1>>27 ^ as ^ B(as)<<11^B(s1^v2)<<9 ^ B(as^s1)<<33^ B(v2^as>>13^as^s1)<<23
+		as = as<<23 ^ as>>3 ^ s1 ^ B(as^v2)<<13^B(s1)<<11 ^ B(as^s1)<<40^ B(v2^as>>9^as^s1)<<17
+		s1 = s1<<5 ^ s1>>3 ^ as ^ B(as)<<15^B(s1^v2)<<5 ^ B(as^s1)<<43^ B(v2^as>>23^as^s1)<<23
+		s2 = s2<<3 ^ s2>>17 ^ s1 ^ B(as^v2)<<17^B(s1)<<2 ^ B(as^s1)<<51^ B(v2^as>>15^as^s1)<<17
+		s1, s2, s3 = s3, s1, s2
 	}
 
 	for i, v2 := range src {
@@ -48,7 +54,7 @@ func (lx LXRHash) Hash(src []byte) []byte {
 	for i, h := range hs {
 		step(h)
 		// Set a byte
-		bytes[i] = lx.ByteMap[as&mk] ^ bytes[i]
+		bytes[i] = b(as) ^ bytes[i]
 	}
 
 	// Return the resulting hash
--- util/shaCompare/testhash.go
+++ util/shaCompare/testhash.go
@@ -129,57 +129,6 @@ func AddByteTest() {
 	}
 }
 
-func BitChangeTest() {
-	var g1 lxr.Gradehash
-	var g2 lxr.Gradehash
-
-	buf := Getbuf()
-	cnt := 0
-
-	last := time.Now().Unix()
-	for x := 0; x < 100000000000; x++ {
-		// Get a new buffer of data.
-		buf = Getbuf()
-
-		// pick one of 64 bytes
-		for i := 0; i < len(buf); i++ {
-			// pick one of 8 bits
-			for j := 0; j < 8; j++ {
-				cnt++
-				// Calculate a bit to flip, and flip it.
-				bit_to_flip := byte(1 << uint(j))
-				buf[i] = buf[i] ^ bit_to_flip
-
-				g1.Start()
-				sv := sha256.Sum256(buf)
-				g1.Stop()
-				g1.AddHash(buf, sv[:])
-
-				g2.Start()
-				wv := lxrhash.Hash(buf)
-				g2.Stop()
-				g2.AddHash(buf, wv)
-
-				// flipping a bit again repairs it.
-				buf[i] = buf[i] ^ bit_to_flip
-
-				if cnt > 1000 && time.Now().Unix()-last > 4 {
-					last = time.Now().Unix()
-					cnt = 0
-
-					c, r1 := g1.Report("bit-sha")
-					_, r2 := g2.Report("bit-lxr")
-					// Print on one line, so if we run multiple tests at the same time, we don't
-					// split the output, because go will ensure one print goes out uninterrupted.
-					fmt.Printf("%10s %s\n%10s %s\n\n", c, r1, " ", r2)
-				}
-			}
-
-		}
-
-	}
-}
-
 func DifferentHashes() {
 	var g1 lxr.Gradehash
 	var g2 lxr.Gradehash
@@ -229,36 +178,89 @@ func GenAll(Seed, Passes uint64) {
 	}
 }
 
+func BitChangeTest() {
+	var g1 lxr.Gradehash
+	var g2 lxr.Gradehash
+
+	buf := Getbuf()
+	cnt := 0
+
+	last := time.Now().Unix()
+	for x := 0; x < 100000000000; x++ {
+		// Get a new buffer of data.
+		buf = Getbuf()
+
+		// pick one of 64 bytes
+		for i := 0; i < len(buf); i++ {
+			// pick one of 8 bits
+			for j := 0; j < 8; j++ {
+				cnt++
+				// Calculate a bit to flip, and flip it.
+				bit_to_flip := byte(1 << uint(j))
+				buf[i] = buf[i] ^ bit_to_flip
+
+				g1.Start()
+				sv := sha256.Sum256(buf)
+				g1.Stop()
+				g1.AddHash(buf, sv[:])
+
+				g2.Start()
+				wv := lxrhash.Hash(buf)
+				g2.Stop()
+				g2.AddHash(buf, wv)
+
+				// flipping a bit again repairs it.
+				buf[i] = buf[i] ^ bit_to_flip
+
+				if true {
+					if cnt > 1000 && time.Now().Unix()-last > 4 {
+						last = time.Now().Unix()
+						cnt = 0
+
+						c, r1 := g1.Report("bit-sha")
+						_, r2 := g2.Report("bit-lxr")
+						// Print on one line, so if we run multiple tests at the same time, we don't
+						// split the output, because go will ensure one print goes out uninterrupted.
+						fmt.Printf("%10s %s\n%10s %s\n\n", c, r1, " ", r2)
+					}
+				} else {
+					if cnt > 100000 {
+						cnt = 0
+
+						c, r1 := g1.Report("bit-sha")
+						_, r2 := g2.Report("bit-lxr")
+						// Print on one line, so if we run multiple tests at the same time, we don't
+						// split the output, because go will ensure one print goes out uninterrupted.
+						fmt.Printf("%10s %s\n%10s %s\n\n", c, r1, " ", r2)
+					}
+
+				}
+			}
+
+		}
+
+	}
+}
+
 func main() {
 	rand.Seed(123412341234)
 
 	Seed := uint64(0xFAFAECECFAFAECEC)
 	MaxSizeBits := uint64(25)
-	Passes := uint64(5)
+	Passes := uint64(6)
 	HashSize := uint64(256)
 
-	lxrhash.Init(Seed, MaxSizeBits, HashSize, Passes)
+	GenAll(Seed,Passes)
 
-	//GenAll(Seed,Passes)
+	lxrhash.Init(Seed, MaxSizeBits, HashSize, Passes)
 
 	lxr.Gradehash{}.PrintHeader()
 
 	//go BitCountTest(rate)
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
-	go BitChangeTest()
+	numTests := 8
+	for i := 0; i < numTests; i++ {
+		go BitChangeTest()
+	}
 
 	//go DifferentHashes(rate)
 	//go AddByteTest(rate)
