e620831cb1d68389a636ce387fadaed632547c0a
Paul Snow <snow.paul@gmail.com>
2019-07-12 08:25:29 -0500

    added headers to all go files for the MIT license

--- lxrhash_test.go
+++ lxrhash_test.go
@@ -1,3 +1,5 @@
+// Copyright (c) of parts are held by the various contributors
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
 package lxr
 
 import "testing"
