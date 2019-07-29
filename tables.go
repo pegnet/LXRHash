51044a7b11ec62838befbed08897cec005015d1c
sambarnes <mistersamuelbarnes@gmail.com>
2019-07-29 08:54:31 -0500

    Changed lrxhash in bytemap filename to lxrhash

--- tables.go
+++ tables.go
@@ -72,7 +72,7 @@ func (lx *LXRHash) ReadTable() {
 		panic(fmt.Sprintf("Could not create the directory %s", lxrhashPath))
 	}
 
-	filename := fmt.Sprintf(lxrhashPath+"/lrxhash-seed-%x-passes-%d-size-%d.dat", lx.Seed, lx.Passes, lx.MapSizeBits)
+	filename := fmt.Sprintf(lxrhashPath+"/lxrhash-seed-%x-passes-%d-size-%d.dat", lx.Seed, lx.Passes, lx.MapSizeBits)
 	// Try and load our byte map.
 	lx.Log(fmt.Sprintf("Reading ByteMap Table %s", filename))
 
