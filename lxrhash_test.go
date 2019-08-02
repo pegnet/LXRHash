10e4a383cf227188d8fbfdb4e27600e230902370
Paul Snow <snow.paul@gmail.com>
2019-08-02 08:05:56 -0500

    actually add new test, update expected hashes (they change with the consideration of v2)

--- lxrhash_test.go
+++ lxrhash_test.go
@@ -45,9 +45,9 @@ func TestKnownHashes(t *testing.T) {
 
 	known := map[string]string{
 		"":       "66afa4d58ff4b99ef77f7bc2dc7567a23ccb47edab1486fccc3e9556bc64e9cc",
-		"foo":    "7dda54f8d5efcd6928870bdc9ece900b320e897bce4814e9010cc08647c197ae",
-		"bar":    "fe2cb7f3cef5702a1cb4712434085afe1efdef1d2563291e4883cd2a3ea1e074",
-		"pegnet": "cd45b08c0619d78e2a810c4e6462296ec51ae4fd0f73a54a154a97a54942297e",
+		"foo":    "2b72065417eaa9304b13521ce82effd183c934f015098ed8adaa9383f10c67c1,",
+		"bar":    "eabbf4bc9279564ae33d398b88f7793c5cf0c9ce32ace0dcb0d9738543545419,,",
+		"pegnet": "7cbf638ae44144a92b249fbd5b91ab25f438d60c21df5e8188d1d3c16e462b4a,,",
 	}
 
 	for k, v := range known {
