c62f4c6f94f07da0a96f5c25f11a02c803233adc
Paul Snow <snow.paul@gmail.com>
2019-10-06 18:22:10 -0500

    Updated test, updated and commented the hash function

--- lxrhash2.go
+++ lxrhash2.go
@@ -2,6 +2,8 @@
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 package lxr
 
+import "bytes"
+
 // LXRHash holds one instance of a hash function with a specific seed and map size
 type LXRHash2 struct {
 	ByteMap     []byte // Integer Offsets
@@ -14,45 +16,76 @@ type LXRHash2 struct {
 	verbose     bool
 }
 
+// Returns just the 32 byte hash.
 func (lx *LXRHash2) Hash(src []byte) []byte {
-	h, _ := lx.HashValidate(src, nil, false)
-	return h
+	h := lx.HashValidate(src, nil)
+	return h[:32]
 }
 
-// Hash takes the arbitrary input and returns the resulting hash of length HashSize
-func (lx *LXRHash2) HashValidate(src []byte, vlist []byte, validate bool) (rt []byte, list []byte) {
+// Hash takes the arbitrary input and returns what amounts to a 256 byte hash.  32 bytes are the literal hash
+// of the data using the ByteMap, and the remaining 196 bytes are the inputs from the ByteMap required to recompute
+// the hash.
+//
+// Takes the source document, and the 256 byte hash.  If the hash is nil, the hash is computed.  If the hash is given
+// then we return the hash if it validates, or a nil if the hash fails validation.
+func (lx *LXRHash2) HashValidate(src []byte, hash []byte) []byte {
+	// The hash to validate must be nil (so we compute it) or length 256 if we are validating the hash.
+	if hash != nil && len(hash) != 256 {
+		return nil
+	}
 	// Since MapSize is specified in bits, the index mask is the size-1
 	mk := lx.MapSize - 1
+	// If we are building a hash, then we will be collecting the bytes into a verfication list.
+	var vlist []byte
 
-	B := func(v uint64) byte { b := lx.ByteMap[v&mk]; list = append(list, b); return b }
-	if validate {
-		B = func(v uint64) (b byte) { b, vlist = vlist[0], vlist[1:]; return b }
+	// Pick a function; assume that we are building a hash, not validating a hash
+	B := func(v uint64) byte { b := lx.ByteMap[v&mk]; vlist = append(vlist, b); return b }
+	if hash != nil {
+		h := hash[32:] // h will point to the validation bytes of the hash
+		// But if it turns out we are validating a hash, not building one, then use the validation byte source
+		B = func(v uint64) (b byte) { b, h = h[0], h[1:]; return b }
 	}
+
+	// The offset variable is all the state we really need for PoW.  The more complex state used in
+	// the origonal LXRHash makes it a better cryptographic hash, but allows computation to play a bigger role
+	// in PoW.
+	//
+	// That said, we have to initialize the offset variable with the full state of the data from the source.  We
+	// are going to make one pass through the data and reduce it to 32 bytes.  Then make a pass through the result
+	// (data the 32 bytes, and the 8 byte offset) to ensure even the last bit has an impact on all bits.
 	var offset uint64
 	data := make([]byte, int(lx.HashSize))
 	s := 0
 	d := 0
-	for i := 0; i < len(src)*3; i++ {
-		if s >= len(src) {
-			s = 0
-		}
-		if d >= int(lx.HashSize) {
-			d = 0
-		}
-		offset = offset<<11 ^ offset>>1 ^ uint64(src[s])<<33 ^ uint64(src[s])>>7
+	for i := 0; i < len(src); i++ {
+		s = s % len(src)
+		d = d % int(lx.HashSize)
+		offset = offset<<11 ^ offset>>1 ^ uint64(src[s])<<40 ^ uint64(src[s])<<16
 		data[d] = byte(offset) ^ data[d]
 		s++
 		d++
 	}
-	p := 0
-	for i := 0; i < 8*int(lx.HashSize); i++ {
-		data[p] = data[p] ^ B(offset+uint64(data[p])) ^ byte(offset^offset>>11^offset>>33)
-		offset = offset<<15 ^ offset>>7 ^ uint64(data[p])<<3 ^ uint64(data[p])<<23
-		p++
-		if p >= int(lx.HashSize) {
-			p = 0
-		}
+	// Because we are now going to process just the 32 bytes that step one reduced our source down, we get a
+	// standard sized byte stream for the validation, no matter how much data we are hashing.
+	for _, b := range data {
+		offset = offset<<11 ^ offset>>1 ^ uint64(b)<<32 ^ uint64(b)
 	}
 
-	return data[:], list
+	p := 0
+	for i := 0; i < 7*int(lx.HashSize); i++ {
+		p = p % len(data)
+		d := data[p]
+		d = d ^ B(offset+uint64(d)) //^ byte(offset^offset>>11^offset>>33)
+		offset = offset<<7 ^ offset>>1 ^ uint64(d)<<3 ^ uint64(d)<<23
+		data[p] = d
+		p++
+	}
+
+	// If we are validating the hash, then return either a nil or the hash we were given.
+	if hash != nil && !bytes.Equal(data[:], hash[:32]) {
+		return nil
+	}
+
+	// If building a list, return the 32 byte hash, with the vlist appended to the hash.
+	return append(data[:], vlist...)
 }
--- testing/sha_compare_BitChange2_test.go
+++ testing/sha_compare_BitChange2_test.go
@@ -3,7 +3,6 @@
 package testing_test
 
 import (
-	"bytes"
 	"crypto/sha256"
 	"fmt"
 	"sync"
@@ -33,63 +32,58 @@ func BitChangeTest2() {
 
 	cnt := int64(0)
 
+	buf := []byte("n3jg498cm89f36y3bbeu5euenekl,tlrwuw4qw4bubu6ebeer76ier6kmrkm6rrkkrem6en5beuwewew6nv9")
+	bit := byte(1)
 	last := time.Now().Unix()
 	for x := int64(0); x < 100000000000; x++ {
-		// Get a new buffer of data.
-		buf := Getbuf(1024)
+		buf[int(x*7)%len(buf)] = buf[int(x*7)%len(buf)] ^ bit
+		if x%13 == 0 {
+			bit++
+			if bit == 0 {
+				bit = 1
+			}
+		}
+		if x%100 == 0 {
+			buf[int(x)%len(buf)]++
+		}
 
-		// pick one of 64 bytes
-		for i := 0; i < len(buf); i++ {
-			// pick one of 8 bits
-			for j := 0; j < 8; j++ {
-				cnt++
-				// Calculate a bit to flip, and flip it.
-				bit_to_flip := byte(1 << uint(j))
-				buf[i] = buf[i] ^ bit_to_flip
+		g1.Start()
+		sv := sha256.Sum256(buf)
+		g1.Stop()
+		g1.AddHash(buf, sv[:])
 
-				g1.Start()
-				sv := sha256.Sum256(buf)
-				g1.Stop()
-				g1.AddHash(buf, sv[:])
+		g2.Start()
+		wv := LX.HashValidate(buf, nil)
+		wv2 := LX.HashValidate(buf, wv)
+		if wv2 == nil {
+			panic("Validate Fails")
+		}
+		g2.Stop()
+		g2.AddHash(buf, wv[:32])
+		once.Do(func() {
+			fmt.Println("vlist len ", len(wv))
+		})
+		cnt++
+		if true {
+			if cnt > 1000 && time.Now().Unix()-last > 4 {
+				last = time.Now().Unix()
+				cnt = 0
 
-				g2.Start()
-				wv, vlist := LX.HashValidate(buf, nil, false)
-				wv2, _ := LX.HashValidate(buf, vlist, true)
-				if !bytes.Equal(wv, wv2) {
-					panic("Validate Fails")
-				}
-				g2.Stop()
-				g2.AddHash(buf, wv)
-				once.Do(func() {
-					fmt.Println("vlist len ", len(vlist))
-				})
+				c, r1 := g1.Report("bit-sha")
+				_, r2 := g2.Report("bit-lxr")
+				// Print on one line, so if we run multiple tests at the same time, we don't
+				// split the output, because go will ensure one print goes out uninterrupted.
+				fmt.Printf("%10s %s\n%10s %s\n\n", c, r1, " ", r2)
+			}
+		} else {
+			if cnt > 100000 {
+				cnt = 0
 
-				// flipping a bit again repairs it.
-				buf[i] = buf[i] ^ bit_to_flip
-
-				if true {
-					if cnt > 1000 && time.Now().Unix()-last > 4 {
-						last = time.Now().Unix()
-						cnt = 0
-
-						c, r1 := g1.Report("bit-sha")
-						_, r2 := g2.Report("bit-lxr")
-						// Print on one line, so if we run multiple tests at the same time, we don't
-						// split the output, because go will ensure one print goes out uninterrupted.
-						fmt.Printf("%10s %s\n%10s %s\n\n", c, r1, " ", r2)
-					}
-				} else {
-					if cnt > 100000 {
-						cnt = 0
-
-						c, r1 := g1.Report("bit-sha")
-						_, r2 := g2.Report("bit-lxr")
-						// Print on one line, so if we run multiple tests at the same time, we don't
-						// split the output, because go will ensure one print goes out uninterrupted.
-						fmt.Printf("%10s %s\n%10s %s\n\n", c, r1, " ", r2)
-					}
-
-				}
+				c, r1 := g1.Report("bit-sha")
+				_, r2 := g2.Report("bit-lxr")
+				// Print on one line, so if we run multiple tests at the same time, we don't
+				// split the output, because go will ensure one print goes out uninterrupted.
+				fmt.Printf("%10s %s\n%10s %s\n\n", c, r1, " ", r2)
 			}
 
 		}
